b1be56da9d90ea2dbcb30ba65dcc98bdb16b9eae
jiuge123 <201211964@qq.com>
2017-11-18 23:57:17 +0800

    alloc

--- HeadFile/alloc.h
+++ HeadFile/alloc.h
@@ -5,9 +5,9 @@ namespace JStl{
 class Alloc_Poor
 {
 private:
-	static const int k_align_ = 8;
-	static const int k_max_bytes_ = 128;
-	static const int k_list_number_ = k_align_ / k_max_bytes_;
+	static const size_t k_align_ = 8;
+	static const size_t k_max_bytes_ = 128;
+	static const size_t k_list_number_ = k_max_bytes_ / k_align_;
 	static size_t round_Up(size_t bytes)
 	{//8的倍数
 		return (bytes + k_align_ - 1) & ~(k_align_ - 1);
@@ -18,9 +18,14 @@ private:
 	};
 	static Free_Node *free_list_[k_list_number_];
 	static size_t free_List_Index(size_t n)
-	{
+	{// 返回索引
 		return (n + k_align_ - 1) / k_align_ - 1 ;
 	}
+	static void* refill(size_t n);
+	static char* chunk_Alloc(size_t size, int &nobjs);
+	static char *start_;
+	static char *end_free_;
+	static char *heap_size_;
 public:
 	Alloc_Poor() = default;
 
@@ -31,6 +36,21 @@ public:
 	Alloc_Poor& operator=(const  Alloc_Poor&) = delete;
 
 };
+
+char* Alloc_Poor::start_ = nullptr;
+char* Alloc_Poor::end_free_ = nullptr;
+char* Alloc_Poor::heap_size_ = nullptr;
+
+Alloc_Poor::Free_Node*
+Alloc_Poor::free_list_[Alloc_Poor::k_list_number_] =
+{
+	nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr,
+	nullptr, nullptr, nullptr, nullptr
+};
+
+
 
 }//namespace JStl
 #endif
