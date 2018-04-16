1148354e9965ef841ff05bfb648fb23e11594cdd
jiuge123 <201211964@qq.com>
2018-04-17 00:45:52 +0800

    pair
    2018/4/17

--- CppFile/源.cpp
+++ CppFile/源.cpp
@@ -3,25 +3,22 @@
 #include"../HeadFile/vector.h"
 #include"../HeadFile/list.h"
 #include"../HeadFile/functional.h"
+#include"../HeadFile/util.h"
 #include<iostream>
 #include<list>
 #include<vector>
-using namespace std;
-template<class T,class T2>
-struct fpair{
-	typedef T first_type ;
-	typedef T2 second_type ;
-	int first =4;
-	double second=9.3;
+using std::cout;
+using std::endl;
+struct MyStruct
+{
+	MyStruct(int a){}
 };
 int main()
 {
 	JStl::list<int> a{ 1, 2 };
 	JStl::vector<int> b{ 1, 2 ,3};
-	list<int> c{ 1, 2 };
+	std::list<int> c{ 1, 2 };
 	JStl::list<int> d{ 1,2 ,3};
-	JStl::selectsecond <  fpair < int, float > > f;
-	fpair<int, float> g;
-	cout << f(g);
+	JStl::pair<int, double> f(2, 3.7);
 	system("pause");
 }
--- HeadFile/util.h
+++ HeadFile/util.h
@@ -63,5 +63,59 @@ T* address_of(T& value)
 	return &value;
 }
 
+//pair
+template<typename Arg1,typename Arg2>
+struct pair{
+	typedef		Arg1	first_type;
+	typedef		Arg2	second_type;
+	first_type first;
+	second_type second;
+
+//构造函数及运算符
+
+	pair() :first(), second()
+	{}
+
+	pair(const Arg1 &a, const Arg2 &b) :first(a), second(b)
+	{}
+
+	pair(Arg1 &&a, Arg2 &&b) :first(forward<first_type>(a)),
+		second(forward<second_type>(b))
+	{}
+
+	pair(const pair& rhs) :first(rhs.first), second(rhs.second)
+	{}
+
+	//两个类型不同的pair转化
+	//如果rhs的类型可以转化为当前
+	template <class Other1, class Other2,
+		typename std::enable_if<
+		std::is_constructible<Arg1, const Other1&>::value &&
+		std::is_constructible<Arg2, const Other2&>::value &&
+		std::is_convertible<const Other1&, Arg1>::value &&
+		std::is_convertible<const Other2&, Arg2>::value, int>::type = 0>
+		pair(const pair<Other1, Other2>& other)
+		: first(other.first),second(other.second)
+	{}
+	
+	pair(pair&& rhs) :first(forward<first_type>(rhs.first)),
+		second(forward<second_type>(rhs.second))
+	{}
+
+	template <class Other1, class Other2,
+		typename std::enable_if<
+		std::is_constructible<Arg1, Other1>::value &&
+		std::is_constructible<Arg2, Other2>::value &&
+		std::is_convertible<Other1, Arg1>::value &&
+		std::is_convertible<Other2, Arg2>::value, int>::type = 0>
+		pair(pair<Other1, Other2>&& other)
+		: first(forward<Other1>(other.first)),
+		second(forward<Other2>(other.second))
+	{}
+
+};
+
+
+
 }//namespace JiugeStl
 #endif
